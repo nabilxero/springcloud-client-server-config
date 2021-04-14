4f85c2d4ead799acbb9e254fb00279abeac298ab
Nabil <nurahasnatnabil@gmail.com>
2021-04-14 16:22:56 +0600

    update: In controller

--- hello-config-client/src/main/java/com/practise/helloconfigclient/controller/TestController.java
+++ hello-config-client/src/main/java/com/practise/helloconfigclient/controller/TestController.java
@@ -18,6 +18,12 @@ public class TestController {
     @Autowired
     private Environment environment;
 
+//    private final Environment environment;
+//
+//    public TestController(Environment environment) {
+//        this.environment = environment;
+//    }
+
     @GetMapping("/hello")
     public ResponseEntity<String> getHello(Model model) {
         return new ResponseEntity<String>(environment.getProperty("message"), HttpStatus.OK);
